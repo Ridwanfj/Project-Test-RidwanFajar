b4310ac3c4bda086b5c13b656bb2e4ce16bf732d
RidwanFajar <ridwanfajar321@gmail.com>
2025-07-13 09:22:35 +0700

    ,

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,31 @@ export default defineConfig({
       '/api': {
         target: 'https://suitmedia-backend.suitdev.com',
         changeOrigin: true,
-        rewrite: path => path.replace(/^\/api/, '')
+        rewrite: path => path.replace(/^\/api/, ''),
+        configure: (proxy, options) => {
+          proxy.on('error', (err, req, res) => {
+            console.log('proxy error', err);
+          });
+          proxy.on('proxyReq', (proxyReq, req, res) => {
+            console.log('Sending Request to the Target:', req.method, req.url);
+          });
+          proxy.on('proxyRes', (proxyRes, req, res) => {
+            console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+          });
+        }
       }
     }
-  }
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html'
+      }
+    },
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true
+  },
+  base: './',
+  publicDir: 'public'
 });
